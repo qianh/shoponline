d5b903a4d07360f0063ea644000bc5f032dede36
junyu <junyu@taobao.com>
2014-10-06 09:52:37 +0800

    liqiang commit

--- src/main/java/com/qh/shop/web/Auth.java
+++ src/main/java/com/qh/shop/web/Auth.java
@@ -3,6 +3,7 @@ package com.qh.shop.web;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+//liqiang test
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
     public String value() default"user";
